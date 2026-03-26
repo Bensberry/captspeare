4de078eda029b5565a8b06c158942d56bc4eb8e0
Ben <benthedark318@gmail.com>
2026-03-27 01:08:50 +0530

    Update test_models.js

--- test_models.js
+++ test_models.js
@@ -1,4 +1,3 @@
-const key = "AIzaSyDWPsfVTnI-Aidgmb5BRbIm6YLsQS_oSFs";
 fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${key}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
